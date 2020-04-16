fd7521466dac951446e765ba28045e3a9fef6869
Chinwe <ugoji.chinwe@gmail.com>
2020-04-16 01:07:54 +0100

    still trying to fix heroku build

--- src/app.js
+++ src/app.js
@@ -15,28 +15,9 @@ const print = debug('dev');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.get('/', (req, res) => res.status(301).redirect('/api/v1/'));
-// app.get('/', (req, res) => {res.send('Hello darkness my old friend!');});
+app.get('/', (_req, res) => res.status(301).redirect('/api/v1/'));
+
 app.use('/api/v1', routes);
-
-app.use('*', (req, res) => {
-  res.status(400).send({ message: 'Bazinga! Wrong route' });
-});
-
-app.use((req, res, next) => {
-  const err = new Error('Not found');
-  err.status = 404;
-  next(err);
-});
-
-app.use((err, req, res, next) => {
-  res.status(err.status || 500);
-  res.json({
-    err: {
-      message: err.message,
-    },
-  });
-});
 
 app.listen(port, () => {
   print(`Listening on port ${chalk.blue(port)}`);
